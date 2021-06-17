6b81bed2ce43f158df700c1e8100c594528484fc
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-17 15:52:45 +0900

    20210617 6교시 수업내용

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,14 +1,19 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -63,6 +68,18 @@ public class SampleController {
 		log.info("" + dto);
 		
 		return "ex01";
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+	}
+
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo: " + todo);
+		return "ex03";
 	}
 	
 }
