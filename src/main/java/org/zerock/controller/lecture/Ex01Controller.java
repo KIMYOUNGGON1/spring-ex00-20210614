c0863705f5f4be3257bc745c67e9d106f93ffe19
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-17 09:56:20 +0900

    20210617 1교시 수업내용

---
+++ src/main/java/org/zerock/controller/lecture/Ex01Controller.java
@@ -0,0 +1,21 @@
+package org.zerock.controller.lecture;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+
+@Controller
+@RequestMapping("/ex01/*")
+@Log4j
+public class Ex01Controller {
+	
+	@RequestMapping("/sub01")
+	public void method01() {
+		//System.out.println("ex01, sub01 method");
+		log.info("ex01, sub01 method");
+	}
+}
