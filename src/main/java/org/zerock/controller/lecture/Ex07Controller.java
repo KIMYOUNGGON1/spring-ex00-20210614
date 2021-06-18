4940a9056d81d9163214c75e0ad41b82c11df738
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-18 11:55:44 +0900

    20210618 3교시 수업내용

--- src/main/java/org/zerock/controller/lecture/Ex07Controller.java
+++ src/main/java/org/zerock/controller/lecture/Ex07Controller.java
@@ -1,8 +1,12 @@
 package org.zerock.controller.lecture;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.zerock.controller.lecture.domain.User;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,6 +22,58 @@ public class Ex07Controller {
 		
 		return "my response message";
 		
+	}
+	
+	@RequestMapping("/sub02")
+	public @ResponseBody String method02() {
+		log.info("ex07, sub02 method");
+		
+		User user = new User();
+		user.setName("donald");
+		user.setAge(99);
+		
+		return user.toString();
+		
+	}
+	
+	@RequestMapping("/sub03")
+	public @ResponseBody String method03() {
+		log.info("ex07, sub03 method");
+		
+		User user = new User();
+		user.setName("donald");
+		user.setAge(99);
+		
+		return "{\"id\":" + "\"" + user.getName() + "\", " + "\"age\":" + user.getAge() + "}";
+	}
+	
+	@RequestMapping("/sub04")
+	public @ResponseBody User method04() {
+		log.info("ex07, sub04 method");
+		
+		User user = new User();
+		user.setName("trump");
+		user.setAge(88);
+		
+		return user;
+	}
+	
+	@RequestMapping("/sub05")
+	public ResponseEntity<String> method05() {
+		log.info("ex07, sub05 method");
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "text/html; charset=utf-8");
+		headers.add("my-header", "my-value" );
+		
+		String body = "<h1>Hello Entity</h1>";
+		
+		// 상태 코드, 값
+		// 부가정보(header)
+		// 본문(body>
+		// ResponseEntity<String> entity 
+				
+		return new ResponseEntity<String>(body, headers, HttpStatus.OK);
 	}
 	
 }
