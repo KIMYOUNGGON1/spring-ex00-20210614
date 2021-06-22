f363f3d2538332631dd58dbd5d8d99a3a7e66da8
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-22 10:53:30 +0900

    20210622 2교시 수업내용

---
+++ src/main/java/org/zerock/service/BoardServieImpl.java
@@ -0,0 +1,47 @@
+package org.zerock.service;
+
+import java.util.List;
+
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class BoardServieImpl implements BoardService{
+
+	private BoardMapper mapper;
+	
+	@Override
+	public void register(BoardVO board) {
+		mapper.insertSelectKey(board);
+		
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		return mapper.getList();
+	}
+	
+}
