818243fba30a611ee933d3171b140cc8ea0754cc
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-06-28 09:43:17 +0900

    20210628 1교시 수업내용

---
+++ src/main/java/org/zerock/controller/lecture/rest/Ex01RestController.java
@@ -0,0 +1,29 @@
+package org.zerock.controller.lecture.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/rest1/*")
+public class Ex01RestController {
+
+	@RequestMapping("/sub01")
+	@ResponseBody
+	public String method01() {
+		
+		return "/rest01/sub01";
+	}
+	
+	@RequestMapping("/sub02")
+	@ResponseBody
+	public String method02(){
+		return "hello world";
+	}
+	
+	@RequestMapping("/sub03")
+	public String method03() {
+		return "someViewName";
+	}
+	
+}
---
+++ src/main/java/org/zerock/controller/lecture/rest/Ex02RestController.java
@@ -0,0 +1,58 @@
+package org.zerock.controller.lecture.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.controller.lecture.domain.User;
+
+
+
+//@Controller
+//@ResponseBody //모든 메소드에 붙인 것고 같음.
+@RestController // @Controller, @ResponseBody 조합
+@RequestMapping("/rest02")
+public class Ex02RestController {
+	
+	@RequestMapping("/sub01")
+	public String method01() {
+		return "response body1";
+	}
+	
+	@RequestMapping("/sub02")
+	public User method02() {
+		User user = new User();
+		user.setName("korea");
+		user.setAge(1);
+		
+		return user;
+	}
+	
+	@RequestMapping("/sub03")
+	public List<String> method03() {
+		List<String> list = new ArrayList<String>();
+		list.add("java");
+		list.add("script");
+		list.add("object");
+		list.add("notation");
+		
+		return list;
+		// return new String[] {"java", "script", "object", "notation"};
+	}
+	
+	@RequestMapping("/sub04")
+	public List<User> method04()  {
+		List<User> list = new ArrayList<User>();
+		User user = new User();
+		user.setName("seoul");
+		user.setAge(3);
+		
+		list.add(user);
+		list.add(user);
+		list.add(user);
+		
+		return list;
+	}
+	
+}
