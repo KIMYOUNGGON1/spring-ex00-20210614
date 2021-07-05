3f8aded8c7b3ef194908b5a9d9bd082d3e85424e
KIMYOUNGGON1 <dudrhs950@naver.com>
2021-07-05 11:02:24 +0900

    20210705 1교시 수업내용

---
+++ src/main/java/org/zerock/controller/lecture/upload/Ex02UploadController.java
@@ -0,0 +1,44 @@
+package org.zerock.controller.lecture.upload;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.log4j.Log4j;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Controller
+@RequestMapping("/uploadex02")
+@Log4j
+public class Ex02UploadController {
+	
+	@RequestMapping("/sub01")
+	public void method01(MultipartFile file) throws Exception{
+		log.info(file.getOriginalFilename());
+	
+	
+		String buckeyName = "choongang-kyg";
+		String profileName = "spring1";
+		S3Client s3 = S3Client.builder()
+				.credentialsProvider(ProfileCredentialsProvider.create(profileName))
+				.build();
+		
+		
+		PutObjectRequest objectRequeset = PutObjectRequest.builder()
+				.bucket(buckeyName)
+				.key(file.getOriginalFilename())
+				.contentType(file.getContentType())
+				.acl(ObjectCannedACL.PUBLIC_READ)
+				.build();
+		
+		s3.putObject(objectRequeset, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+		
+		log.info("done");
+	}
+	
+	
+}
